79a697ebe9ddbc950f61cef600a1c9e2c8b6ce76
Karol Czeryna <k@e-dot.uk>
2021-02-25 10:30:20 +0000

    Add logo and format all documents

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,36 @@
-document.addEventListener('DOMContentLoaded', function (event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   // array with texts to type in typewriter
-  var dataText = ["London.", "DevOps.", "Backend.", "Architecting.", "Planning.", "Consulting."];
+  var dataText = [
+    "London.",
+    "DevOps.",
+    "Backend.",
+    "Architecting.",
+    "Planning.",
+    "Consulting.",
+  ];
 
   // type one text in the typwriter
   // keeps calling itself until the text is finished
   function typeWriter(text, i, fnCallback) {
     // chekc if text isn't finished yet
-    if (i < (text.length)) {
+    if (i < text.length) {
       // add next character to h1
       document.querySelector("h1").innerHTML = text.substring(0, i + 1);
 
       // wait for a while and call this function again for next character
       setTimeout(function () {
-        typeWriter(text, i + 1, fnCallback)
+        typeWriter(text, i + 1, fnCallback);
       }, 100);
     }
     // text finished, call callback if there is a callback function
-    else if (typeof fnCallback == 'function') {
+    else if (typeof fnCallback == "function") {
       // call callback after timeout
       setTimeout(fnCallback, 700);
     }
   }
   // start a typewriter animation for a text in the dataText array
   function StartTextAnimation(i) {
-    if (typeof dataText[i] == 'undefined') {
+    if (typeof dataText[i] == "undefined") {
       setTimeout(function () {
         StartTextAnimation(0);
       }, 20000);
